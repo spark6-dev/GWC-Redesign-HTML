59a5d3bbe8e25cf327beed1c0b64582357914037
Henry Lai <hlai@surgeforward.com>
2022-08-15 00:16:40 -0600

    Fix footer-text field name, fix spacing in mobile view

--- goodwater/template-parts/genesis/footer-text.php
+++ goodwater/template-parts/genesis/footer-text.php
@@ -1,5 +1,5 @@
 <?php if( get_field('footer-text') ):?>
     <div class="container">
-        <footer class="footer-text"><?php echo esc_html(get_field('nfooter'));?></footer>
+        <footer class="footer-text"><?php echo esc_html(get_field('footer-text'));?></footer>
     </div>
 <?php endif;?>
