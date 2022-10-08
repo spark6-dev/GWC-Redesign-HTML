92887fb71f03adca49c6fb7b48971be7e5c82ab1
Henry Lai <hhklai@gmail.com>
2022-10-08 09:39:25 -0600

    Add story section, add styling for ipad view

--- goodwater/template-parts/company/story.php
+++ goodwater/template-parts/company/story.php
@@ -4,7 +4,50 @@
             <span class="l-caption"><?php echo esc_html(get_field('story_label'));?></span>
         <?php endif;?>
         <div class="story__wrap">
-            <?=get_field('story_body')?>
+<!--            --><?//=get_field('story_body')?>
+            <div>
+                <div class="tc-heading">
+                    <h3>What if wealth building opportunities were more accessible for everyday Americans?</h3>
+                    <p>That’s the question Brandon Krieg and Ed Robinson asked one another while working on Wall Street. And the more they thought about it, the more they believed they could do something to create change.</p>
+                </div>
+                <div>
+                    <div class="pb-2 center medium-hide is--mobile">
+                        <img src="http://localhost/goodwatercadev/wp-content/uploads/2022/10/stash_story_1_mobile-269x300.jpg" alt="" />
+                    </div>
+                    <div class="story_1_wrapper">
+                        <div>
+                            <div class="story_1_wrapper_medium_flex">
+                                <div class="medium-show is--mobile">
+                                    <img src="http://localhost/goodwatercadev/wp-content/uploads/2022/10/stash_story_1.jpg" alt=""/>
+                                </div>
+                                <div>
+                                    <p>Working at a big investment bank was interesting. Fast-paced. Demanding. Fascinating. But during their time on Wall Street, Brandon Krieg and Ed Robinson realized that the system wasn’t set up for average people. <em>Why</em>, they thought, <em>does the financial system continually box out folks who don’t have a lot of money?</em> And after a few more discussions with one another, they decided to change the landscape.</p>
+                                </div>
+                            </div>
+                            <p>Their realization was simple: the wealth creation systems of American are not set up for average Americans. People don’t invest because they don’t have access to investment vehicles. But with the right tools, education, and opportunities—more people could get through the door to investing opportunities, and start building more stable financial futures.</p>
+                            <p><em class="underline">That idea became Stash</em>. It started as just the two of them, and now it’s grown to a company of over 500 people, working to help lower the barrier of entry to financial education and investment opportunities.</p>
+                        </div>
+                        <div class="is--desktop">
+                            <img src="http://localhost/goodwatercadev/wp-content/uploads/2022/10/stash_story_1.jpg" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div class="tc-left-hug-img">
+                    <img src="http://localhost/goodwatercadev/wp-content/uploads/2022/10/stash_story_2.jpg" alt="" />
+                    <h3>Stash’s founders set out with a seemingly simple goal: to empower regular Americans to build wealth.</h3>
+                </div>
+                <div class="small">
+                    <h3>Stash is a mission-oriented company. And that shows up in a lot of ways.</h3>
+                    <p>As a company, Stash’s goal is to make investing easy and affordable for everyone. They believe you shouldn’t have to already have money to make money. But they intentionally didn’t structure their business as a non-profit. Instead they built a viable financial services company, one that already has over 6 million customers, and is set to scale exponentially.</p>
+                    <p>Their values-driven attitude shows up in a lot of ways. It’s obvious in their diverse and inclusive company workplace. It shows up in their offerings, which help people invest, even if they only have a few dollars to spare. And it shows up in their partnerships—such as the ones they’ve been undertaking with <span class="underline">The Goodwater Collective</span> this year.</p>
+                    <p>One partnership has been a partnership with <span class="underline">YearUp</span>, designed to introduce students to the basics of investing by offering them $1,000 to invest in Smart Portfolio, a set-it-and-forget-it managed investment account. The other is a partnership with <span class="underline">Brooklyn Bank</span>, a non-profit that aims to foster financial independence in communities of color—together they hosted a Black Money Forum for thousands of people in New York.</p>
+                </div>
+                <div class="desktop-up-flex-section small">
+                    <img src="http://localhost/goodwatercadev/wp-content/uploads/2022/10/stash_story_3.jpg" alt="" />
+                    <p>Stash started small, with just a few offerings. But now, seven years into their journey, they offer a range of financial services, from IRAs to banking, and even investment products designed to help improve financial literacy for kids. And if you ask Brandon and Ed, they’ll say they’re still just scratching the surface. They are continually exploring new ways to support financial literacy, to improve access to investments, and more. They encourage investors to diversify, to invest little-by-little, and to get familiar with investing by actually taking the leap and becoming a part of the financial system.</p>
+                </div>
+
+            </div>
         </div>
     </div>
 </div>
